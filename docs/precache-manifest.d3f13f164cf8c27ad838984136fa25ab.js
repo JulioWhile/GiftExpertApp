eb86d4cd433325d40e0425b3b91e1ed17ad8a9e6
Julio Contreras <julio.contreras0429@gmail.com>
2020-06-22 21:32:51 -0700

    docs agregado

---
+++ docs/precache-manifest.d3f13f164cf8c27ad838984136fa25ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d04979db322fe9889f8b79493e6ca89b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d27105fa40c4fcab5b1b",
+    "url": "/static/css/main.f6a3ab63.chunk.css"
+  },
+  {
+    "revision": "3bd7f3839b4867d08700",
+    "url": "/static/js/2.372dfa45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.372dfa45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27105fa40c4fcab5b1b",
+    "url": "/static/js/main.5a9d8e0c.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
